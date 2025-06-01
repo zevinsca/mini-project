691db42acce9ac67da70af29a4e860ab9e376cca
nahlandyza <nahla.damayanti@gmail.com>
2025-06-01 18:49:12 +0700

    add new page

---
+++ apps/web/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <main className="min-h-screen grid place-items-center">
+      <h1>Welcome, Dashboard!</h1>
+    </main>
+  );
+}
--- apps/web/src/components/logout.tsx
+++ apps/web/src/components/logout.tsx
@@ -2,7 +2,7 @@
 
 import { FiLogOut } from "react-icons/fi";
 
-export default function SignOut() {
+export default function LogOut() {
   async function handleClick() {
     try {
       await fetch("http://localhost:8000/api/v1/auth/logout", {
@@ -22,9 +22,9 @@ export default function SignOut() {
   return (
     <button
       onClick={handleClick}
-      className="flex items-center justify-center gap-2 w-full py-2 rounded-xl bg-red-600 hover:bg-red-700 transition text-white font-semibold shadow-md"
+      className="flex items-center justify-center gap-2 w-full px-2 py-2 rounded-xl bg-red-600 hover:bg-red-700 transition text-white font-semibold shadow-md"
     >
-      <FiLogOut size={18} />
+      <FiLogOut size={16} />
       Sign Out
     </button>
   );
